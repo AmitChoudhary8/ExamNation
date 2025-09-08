8432f821505befc9a549c516679b35f8f4fc05da
AmitChoudhary8 <info.amitsihag@gmail.com>
2025-09-08 10:32:24 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import AboutUs from './pages/AboutUs';
 import Footer from './components/Footer';
 // Add this import
 import SupportUs from './pages/SupportUs';
-import AdminDashboard from './pages/admin/AdminDashboard';
+import AdminDashboard from '/src/pages/admin/AdminDashboard';
 import './App.css';
 
 function App() {
